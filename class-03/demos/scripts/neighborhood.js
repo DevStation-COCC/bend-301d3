2cff2c1202a7bf1283a19faa296f0e9dadd0511a
Calvin Cheng <calvincheng919@gmail.com>
2019-10-30 15:58:49 -0700

    class-03

--- class-03/demos/scripts/neighborhood.js
+++ class-03/demos/scripts/neighborhood.js
@@ -4,25 +4,26 @@ let neighborhoods = [];
 
 // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
 function Neighborhood (rawDataObject) {
-  this.name = rawDataObject.name;
-  this.city = rawDataObject.city;
-  this.population = rawDataObject.population;
-  this.founded = rawDataObject.founded;
-  this.body = rawDataObject.body;
+
+  for (let key in rawDataObject){
+    this[key] = rawDataObject[key];
+  }
 }
 
 Neighborhood.prototype.toHtml = function() {
 
   // 1. Get the template from the HTML document
-
+  let template = $('#neighborhood-template').html();
   // 2. Use Handlebars to "compile" the HTML
-
+  let templateRender = Handlebars.compile(template);
   // 3. Do not forget to return the HTML from this method
-
+  return templateRender(this);
 };
-
+//take dataset iterms
 neighborhoodDataSet.forEach(neighborhoodObject => {
+  neighborhoods.push(new Neighborhood(neighborhoodObject));
 });
 
 neighborhoods.forEach(ourNewNeighborhoodObject => {
+  $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
 });
