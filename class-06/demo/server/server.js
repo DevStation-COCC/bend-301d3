49f32981b4a5bf12b0171957f42e94f261a65e94
Calvin Cheng <calvincheng919@gmail.com>
2019-11-04 16:39:54 -0800

    class-06

---
+++ class-06/demo/server/server.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Application Setup
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+app.get('/', (request,response) => {
+  response.send('Home Page!');
+});
+
+app.get('/bad', (request,response) => {
+  throw new Error('poo');
+});
+
+// The callback can be a separate function. Really makes things readable
+app.get('/about', aboutUsHandler);
+
+function aboutUsHandler(request,response) {
+  response.status(200).send('About Us Page');
+}
+
+// API Routes
+app.get('/location', (request,response) => {
+  try {
+    const geoData = require('./data/geo.json');
+    const city = request.query.data;
+    const locationData = new Location(city,geoData);
+    response.send(locationData);
+  }
+  catch(error) {
+    errorHandler('So sorry, something went wrong.', request, response);
+  }
+});
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+// HELPER FUNCTIONS
+
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+
+
+function  notFoundHandler(request,response) {
+  response.status(404).send('huh?');
+}
+
+function errorHandler(error,request,response) {
+  response.status(500).send(error);
+}
+
+
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`) );
